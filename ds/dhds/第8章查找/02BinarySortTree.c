9431490136df9cf5ba60f9239c7eacad6dcaa458
lifeng <Success321>
2017-08-27 23:21:50 +0800

    dd

--- ds/dhds/第8章查找/02BinarySortTree.c
+++ ds/dhds/第8章查找/02BinarySortTree.c
@@ -12,7 +12,7 @@
 typedef int Status;	/* Status是函数的类型,其值是函数结果状态代码，如OK等 */ 
 
 /* 二叉树的二叉链表结点结构定义 */
-typedef  struct BiTNode	/* 结点结构 */
+typedef struct BiTNode	/* 结点结构 */
 {
 	int data;	/* 结点数据 */
 	struct BiTNode *lchild, *rchild;	/* 左右孩子指针 */
