839270708b136d19692e76da987bae43a5963f10
lifeng <Success321>
2017-08-27 23:03:09 +0800

    static search

---
+++ ds/StaticSearch/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  StaticSearch
+//
+//  Created by lifeng on 2017/8/27.
+//  Copyright © 2017年 lifeng. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
